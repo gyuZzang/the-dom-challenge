2540a25927d280af11860cb072c11a2b7c91ca89
Rosa Kim <rosa.kim@goorm.io>
2022-07-01 07:19:11 +0000

    feat(star-rating): 마우스 호버했다 풀면 최근 클릭 값으로 보이게

--- star-rating/script.js
+++ star-rating/script.js
@@ -8,7 +8,6 @@
 function fillStar(elements, n) {
   for (el of elements) {
     //비교연산을 하는게 득인가 실인가
-    console.log(el, n);
     if (n > 0) {
       el.className = "fa fa-star";
     } else {
@@ -20,6 +19,8 @@ function fillStar(elements, n) {
 
 function Star(el, count, callback) {
   const $el = document.querySelector(el);
+	
+  let countStar = 0;
 
   for (let i = 0; i < count; i++) {
     const star = document.createElement("i");
@@ -29,10 +30,13 @@ function Star(el, count, callback) {
 
     star.addEventListener("mouseover", () => {
       fillStar($el.children, i + 1);
-
-      star.addEventListener("click", () => {
-        callback(i + 1);
-      });
+	});
+	star.addEventListener("mouseout", () => {
+      fillStar($el.children, countStar);
+	});
+	star.addEventListener("click", () => {
+	  countStar = i + 1 
+      callback(countStar);
     });
   }
 }
